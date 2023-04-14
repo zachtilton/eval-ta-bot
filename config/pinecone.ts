dd13ab5ca09e8bb06fcb03d1fd9741b23edc2703
zachtilton <zdtilton@gmail.com>
2023-04-14 12:53:05 -0400

    ingested 16 files from Logic of Evaluation copy

--- config/pinecone.ts
+++ config/pinecone.ts
@@ -8,6 +8,6 @@ if (!process.env.PINECONE_INDEX_NAME) {
 
 const PINECONE_INDEX_NAME = process.env.PINECONE_INDEX_NAME ?? 'chatbot1';
 
-const PINECONE_NAME_SPACE = 'evaluation education'; //namespace is optional for your vectors
+const PINECONE_NAME_SPACE = 'logic of eval'; //namespace is optional for your vectors
 
 export { PINECONE_INDEX_NAME, PINECONE_NAME_SPACE };
